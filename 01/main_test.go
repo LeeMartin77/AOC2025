0bd7200cfe3d574ab242fbf6c4c757b40d6e315d
Lee Martin <lee.martin@quotech.io>
2025-12-05 09:37:03 +0000

    feat: 01-1

---
+++ 01/main_test.go
@@ -0,0 +1,29 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/LeeMartin77/AOC2024/00/solution"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPhaseOne(t *testing.T) {
+	teststring := `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+	res := solution.ComputeSolutionOne([]byte(teststring))
+	assert.Equal(t, int64(3), res)
+}
+
+func xTestPhaseTwo(t *testing.T) {
+	teststring := ``
+	res := solution.ComputeSolutionTwo([]byte(teststring))
+	assert.Equal(t, int64(0), res)
+}
