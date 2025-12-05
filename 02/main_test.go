8af3429a435a251a501c977cd6b5a501faf4929e
Lee Martin <hello@leejohnmartin.co.uk>
2025-12-05 20:43:12 +0000

    feat: 02-02

--- 02/main_test.go
+++ 02/main_test.go
@@ -13,8 +13,8 @@ func TestPhaseOne(t *testing.T) {
 	assert.Equal(t, int64(1227775554), res)
 }
 
-func xTestPhaseTwo(t *testing.T) {
-	teststring := ``
+func TestPhaseTwo(t *testing.T) {
+	teststring := `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
 	res := solution.ComputeSolutionTwo([]byte(teststring))
-	assert.Equal(t, int64(0), res)
+	assert.Equal(t, int64(4174379265), res)
 }
--- 02/solution/solution.go
+++ 02/solution/solution.go
@@ -39,5 +39,47 @@ func ComputeSolutionOne(data []byte) int64 {
 }
 
 func ComputeSolutionTwo(data []byte) int64 {
-	panic("unimplemented")
+	datastr := string(data)
+	chnks := strings.Split(datastr, ",")
+	ranges := [][]int64{}
+	for _, chnk := range chnks {
+		ends := strings.Split(chnk, "-")
+		sint, _ := strconv.ParseInt(ends[0], 10, 64)
+		eint, _ := strconv.ParseInt(ends[1], 10, 64)
+
+		ranges = append(ranges, []int64{sint, eint})
+	}
+	invalidids := []int64{}
+	for _, rng := range ranges {
+		for i := range rng[1] - rng[0] + 1 {
+			vl := rng[0] + i
+			stid := fmt.Sprintf("%d", vl)
+			modulos := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+
+		outer:
+			for _, m := range modulos {
+
+				if len(stid)%m == 0 {
+					s, e := 0, len(stid)/m
+					gp := e
+					prt := stid[s:e]
+					for s < len(stid) {
+						if stid[s:e] != prt {
+							continue outer
+						}
+						s += gp
+						e += gp
+					}
+
+					invalidids = append(invalidids, vl)
+					break outer
+				}
+			}
+		}
+	}
+	sum := int64(0)
+	for _, vl := range invalidids {
+		sum += vl
+	}
+	return sum
 }
