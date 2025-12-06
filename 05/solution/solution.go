528415879ab81cc15ebeaeafb5b250376265bf89
Lee Martin <hello@leejohnmartin.co.uk>
2025-12-06 10:35:13 +0000

    feat: 05-01

--- 05/solution/solution.go
+++ 05/solution/solution.go
@@ -5,9 +5,22 @@ import (
 	"strings"
 )
 
+type unspoiltranges struct {
+	ranges [][]int64
+}
+
+func (u *unspoiltranges) IsUnspoilt(p int64) bool {
+	for _, r := range u.ranges {
+		if p >= r[0] && p <= r[1] {
+			return true
+		}
+	}
+	return false
+}
+
 func ComputeSolutionOne(data []byte) int64 {
 	ingredients := false
-	unspoilt := map[int64]bool{}
+	unspoilt := unspoiltranges{}
 	unspoilting := []int64{}
 	for _, ln := range strings.Split(string(data), "\n") {
 		if ln == "" {
@@ -18,13 +31,10 @@ func ComputeSolutionOne(data []byte) int64 {
 			str, end := strings.Split(ln, "-")[0], strings.Split(ln, "-")[1]
 			strint, _ := strconv.ParseInt(str, 10, 64)
 			eint, _ := strconv.ParseInt(end, 10, 64)
-			for strint <= eint {
-				unspoilt[strint] = true
-				strint++
-			}
+			unspoilt.ranges = append(unspoilt.ranges, []int64{strint, eint})
 		} else {
 			strint, _ := strconv.ParseInt(ln, 10, 64)
-			if unspoilt[strint] {
+			if unspoilt.IsUnspoilt(strint) {
 				unspoilting = append(unspoilting, strint)
 			}
 		}
