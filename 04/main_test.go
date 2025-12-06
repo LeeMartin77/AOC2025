01d741aa1022dad34513904f1f9bdec97fc76ad5
Lee Martin <hello@leejohnmartin.co.uk>
2025-12-06 06:45:11 +0000

    feat: 04-01

---
+++ 04/main_test.go
@@ -0,0 +1,29 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/LeeMartin77/AOC2024/04/solution"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPhaseOne(t *testing.T) {
+	teststring := `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+	res := solution.ComputeSolutionOne([]byte(teststring))
+	assert.Equal(t, int64(13), res)
+}
+
+func xTestPhaseTwo(t *testing.T) {
+	teststring := ``
+	res := solution.ComputeSolutionTwo([]byte(teststring))
+	assert.Equal(t, int64(0), res)
+}
---
+++ 04/solution/solution.go
@@ -0,0 +1,48 @@
+package solution
+
+import (
+	"strings"
+)
+
+func ComputeSolutionOne(data []byte) int64 {
+	mapstr := string(data)
+	mp := map[int]map[int]bool{}
+	lines := strings.Split(mapstr, "\n")
+	for y, ln := range lines {
+		for x, v := range ln {
+			if y == 0 {
+				mp[x] = map[int]bool{}
+			}
+			if v == '@' {
+				mp[x][y] = true
+			}
+		}
+	}
+	adjlmt := 4
+	moveables := [][]int{}
+	for x, mpln := range mp {
+		for y := range mpln {
+			adj := 0
+			for _, xoff := range []int{-1, 0, 1} {
+
+				for _, yoff := range []int{-1, 0, 1} {
+
+					if xoff == 0 && yoff == 0 {
+						continue
+					}
+					if mp[x+xoff][y+yoff] {
+						adj += 1
+					}
+				}
+			}
+			if adj < adjlmt {
+				moveables = append(moveables, []int{x, y})
+			}
+		}
+	}
+	return int64(len(moveables))
+}
+
+func ComputeSolutionTwo(data []byte) int64 {
+	panic("unimplemented")
+}
