d87678a9f408a46ad7f137952fabbdb22d24db10
Lee Martin <hello@leejohnmartin.co.uk>
2025-12-06 11:32:03 +0000

    wip: 06-02
    I'm too stupid for this this morning

--- 06/main_test.go
+++ 06/main_test.go
@@ -16,8 +16,11 @@ func TestPhaseOne(t *testing.T) {
 	assert.Equal(t, int64(4277556), res)
 }
 
-func xTestPhaseTwo(t *testing.T) {
-	teststring := ``
+func TestPhaseTwo(t *testing.T) {
+	teststring := `123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  `
 	res := solution.ComputeSolutionTwo([]byte(teststring))
-	assert.Equal(t, int64(0), res)
+	assert.Equal(t, int64(3263827), res)
 }
--- 06/solution/solution.go
+++ 06/solution/solution.go
@@ -1,6 +1,7 @@
 package solution
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -63,5 +64,75 @@ outer:
 }
 
 func ComputeSolutionTwo(data []byte) int64 {
-	panic("unimplemented")
+	strin := string(data)
+	column_indexes := []int{}
+
+	lines := strings.Split(strin, "\n")
+outer:
+	for i, c := range lines[0] {
+		if c == ' ' {
+			for _, ln := range lines {
+				if ln[i] != ' ' {
+					continue outer
+				}
+			}
+			column_indexes = append(column_indexes, i)
+		}
+	}
+	column_indexes = append(column_indexes, len(lines[0]))
+	columns := [][]string{}
+	prev := 0
+	for _, c := range column_indexes {
+		chunks := []string{}
+		for _, line := range lines {
+			chunks = append(chunks, line[prev:c])
+		}
+
+		fmt.Println(chunks)
+		chunklen := len(chunks[0])
+		recomb := make([]string, len(chunks))
+		for i := range chunklen {
+			for _, c := range chunks {
+				fmt.Println(c)
+				if c[i] == ' ' {
+					continue
+				}
+				if c[i] == '+' || c[i] == '*' {
+					recomb = append(recomb, strings.Trim(c, " "))
+					continue
+				}
+				recomb[i] = recomb[i] + string(c)
+			}
+		}
+		columns = append(columns, recomb)
+
+		prev = c
+	}
+	total := int64(0)
+	for _, col := range columns {
+		operator := col[len(col)-1]
+		numbersstr := col[:len(col)-1]
+		numbers := []int64{}
+		for _, nums := range numbersstr {
+			n, _ := strconv.ParseInt(nums, 10, 64)
+			numbers = append(numbers, n)
+		}
+		intotal := int64(0)
+		if operator == "*" {
+			for _, n := range numbers {
+				if intotal == 0 {
+					intotal = n
+				} else {
+					intotal = intotal * n
+				}
+			}
+		}
+		if operator == "+" {
+			for _, n := range numbers {
+				intotal += n
+			}
+		}
+		total += intotal
+	}
+	return total
 }
